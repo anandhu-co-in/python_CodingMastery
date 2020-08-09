6b95b778263e811d556fcd77cbf9ea5a39f8a8a6
codeandcoins <mail.anandhu@gmail.com>
2020-08-09 16:52:47 +0530

    Added two new problem to bucket

--- 01.CodingMasterty/0000.HelloWorld.py
+++ 01.CodingMasterty/0000.HelloWorld.py
@@ -1,26 +1,34 @@
 # Target - Solve 100 Variety of Coding Problems
 
 
-#FOR WHAT?
+# FOR WHAT?
 
-#---> To make myself happy
-#---> To get better at coding
-#thats it!
+# ---> To make myself happy
+# ---> To get better at coding
+# thats it!
 
-#Will i be disappointed if i didnt get any job opertunity even though i completed this ? :
-#---> Absolutely NOT!! :P
+# Will i be disappointed if i didnt get any job opertunity even though i completed this ? :
+# ---> Absolutely NOT!! :P
 
-#Never get Overwhelmed. You have a a well defined Goal. It is achievable!!!
+# Never get Overwhelmed. You have a a well defined Goal. It is achievable!!!
 
 
-#Rules
+# Rules
 
-#Only self coded answers will count here
-#A problem is said to be complete if you do it entirely from scratch. You r not allowed to copy parts, even if same from another file
-#Copied codes doesnt count, unless you really work it out on your own
-#Please store references about where you fround those problems from
-#If you use separate concepts to solve same problems, feel free to count them separately - Ie same problem using Recursion,Dynamic Programming
-#Get stuck for long time?:D. Leave it and come back later! Thats perefectly Natural
+# Only self coded answers will count here
+# A problem is said to be complete if you do it entirely from scratch. You r not allowed to copy parts, even if same from another file
+# Copied codes doesnt count, unless you really work it out on your own
+# Please store references about where you fround those problems from
+# If you use separate concepts to solve same problems, feel free to count them separately - Ie same problem using Recursion,Dynamic Programming
+# Get stuck for long time?:D. Leave it and come back later! Thats perefectly Natural
 
 
 # Count --> 27 ie 27% is done!!
+
+
+# DFS
+# BFS
+# Matching Parenthesis
+# Making use of HashTables , for keeping track of already visited
+# How to manipulate multiple Variables/Pointers at once
+# Find Longest Palindromic Substring in a string
