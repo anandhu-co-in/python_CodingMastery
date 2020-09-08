80de8eace8bd336d87cc4c5327909a1365bbc811
codeandcoins <mail.anandhu@gmail.com>
2020-09-08 14:25:55 +0530

    commit from legion

---
+++ 01.CodingMasterty/8888.FindLongestPalindromicSubstringInAString.py
@@ -0,0 +1,20 @@
+# Find the longest palindromic sub string from a string
+
+
+def find(a):
+
+    longestPalin=None
+    def expandoutward(pointer1, pointer2):
+        pass
+
+    for i in range(0, len(a)):
+
+        if expandoutward(i,i):
+
+
+
+
+        pass
+
+
+print(find("malayalam"))
