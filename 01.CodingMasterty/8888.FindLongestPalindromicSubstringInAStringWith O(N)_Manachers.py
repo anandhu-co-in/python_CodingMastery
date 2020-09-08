81c900a1732768be7526df720fcdf098552b16aa
codeandcoins <mail.anandhu@gmail.com>
2020-09-08 18:05:38 +0530

    created 8888 file for manachers

---
+++ 01.CodingMasterty/8888.FindLongestPalindromicSubstringInAStringWith O(N)_Manachers.py
@@ -0,0 +1,3 @@
+# Given A String, you need to find the Largest Palindromic Substring inside it
+# Already done in no 40, but this time do it in O(n) time
+# https://www.youtube.com/watch?v=nbTSfrEfo6M
