7af700c206a8af6054a33e1cb47a485fe8e3380a
Anandhukrishnan V <Anandhukrishnan.V@gds.ey.com>
2020-10-12 11:50:24 +0530

    Javascript code copypasted below

--- 01.CodingMasterty/8888.FindNoOfZeros.py
+++ 01.CodingMasterty/8888.FindNoOfZeros.py
@@ -4,6 +4,7 @@ import math
 # arr = [1, 1, 1, 1, 0, 0, 0, 0, 0]
 arr = [1,1,0,0,0,0,0]
 
+
 start = 0
 end = len(arr) - 1
 if len(arr) == 0 or arr[len(arr) - 1] == 1:
@@ -15,10 +16,49 @@ else:
     while not (arr[mid] == 1 and arr[mid + 1] == 0):
 
         if arr[mid + 1] == 1:
-            start = mid + 1
+            start = mid
         elif arr[mid] == 0:
-            end = end - 1
+            end = mid
 
         mid = math.floor((start + end) / 2)
 
     print(len(arr) - mid - 1)
+
+
+
+
+
+# function countZeroes(arr){
+#    var n=arr.length;
+#    var start=0;
+#    var end=n-1;
+#
+#    if(n==0){
+#        return 0;
+#    }
+#
+#    if(arr[n-1]==1){
+#        return 0;
+#    }
+#
+#    if(arr[0]==0){
+#        return n;
+#    }
+#
+#    var mid=Math.floor((start+end)/2);
+#    while (start<=end && !(arr[mid]==1 && arr[mid+1]==0)){
+#        1234;
+#
+#        if(arr[mid+1]==1){
+#            start=mid;
+#        }
+#        else{
+#            end=mid;
+#        }
+#     mid=Math.floor((start+end)/2);
+#
+#    }
+#
+# return n-mid-1;
+#
+# }
